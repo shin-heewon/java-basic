fe1a971fada95b3e604e39190d37771aff26ff29
shin-heewon <ise0305@naver.com>
2024-07-03 13:26:51 +0900

    반복문 형성평가 10번 푸는 중

--- src/Jongol/array2/Assessment9.java
+++ src/Jongol/array2/Assessment9.java
@@ -13,7 +13,7 @@ public class Assessment9 {
 
     for (int i=0; i<input; i++){
       for (int j=0; j<input-i; j++){
-        System.out.print(num+" ");
+
       }
 
       for (int j=0; j<input-1; j++){
